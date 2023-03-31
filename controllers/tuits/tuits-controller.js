aca60fa9c61664c14d416945f74247947c77769c
rxjiang <jiang.rux@northeastern.edu>
2023-03-30 18:49:25 -0700

    .

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -4,8 +4,12 @@ let tuits = posts;
 const createTuit = (req, res) => {
   const newTuit = req.body;
   newTuit._id = new Date().getTime() + "";
+  newTuit.replies = 0;
+  newTuit.retuits = 0;
   newTuit.likes = 0;
   newTuit.liked = false;
+  newTuit.dislikes = 0;
+  newTuit.disliked = false;
   tuits.push(newTuit);
   res.json(newTuit);
 };
